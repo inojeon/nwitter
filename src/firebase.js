b534394a1a68fe706c5a63f731abdab6245e8fee
Jeon InHo <inojeon@kisti.re.kr>
2020-09-16 14:41:48 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDP4QrOaOio_t7EkLx3cSNxCOA3OQIgjVw",
+  authDomain: "nwitter-a42b9.firebaseapp.com",
+  databaseURL: "https://nwitter-a42b9.firebaseio.com",
+  projectId: "nwitter-a42b9",
+  storageBucket: "nwitter-a42b9.appspot.com",
+  messagingSenderId: "188158327306",
+  appId: "1:188158327306:web:22895bb8645be93311fdf3"
+};
+export default firebase.initializeApp(firebaseConfig);
+ 
