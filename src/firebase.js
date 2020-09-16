b32c8d82381e9652e324bd30d341fd48ab96aa89
inojeon <inojeon@kisti.re.kr>
2020-09-16 14:58:04 +0900

    Create .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP4QrOaOio_t7EkLx3cSNxCOA3OQIgjVw",
-  authDomain: "nwitter-a42b9.firebaseapp.com",
-  databaseURL: "https://nwitter-a42b9.firebaseio.com",
-  projectId: "nwitter-a42b9",
-  storageBucket: "nwitter-a42b9.appspot.com",
-  messagingSenderId: "188158327306",
-  appId: "1:188158327306:web:22895bb8645be93311fdf3"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 export default firebase.initializeApp(firebaseConfig);
  
