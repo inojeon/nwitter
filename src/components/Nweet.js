abf4f23178e38b929cc3e2d8cc28a24e91e5809d
k1658p2 <k1658p2@k1658p2ui-iMac-Pro.local>
2020-09-22 18:26:44 +0900

    5.0 ProFile

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,14 +1,16 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 
 const Nweet = ( {nweetObj, isOwner }) => {
   const [editing, setEditing] = useState(false);
   const [newNweet, setNewNweet] = useState(nweetObj.text);
+  const [attachmentUrl, setAttachmentUrl] = useState(nweetObj.attachmentUrl)
 
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you Sure you want to delete this nweet?")
     if(ok) {
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     } 
   }
   const toggleEditing = () => {
@@ -40,6 +42,11 @@ const Nweet = ( {nweetObj, isOwner }) => {
       ) : (
         <>
           <h4>{nweetObj.text}</h4>
+          {
+            attachmentUrl && (
+              <img src={nweetObj.attachmentUrl} width="50px" height="50px" />
+            )
+          }
           {
             isOwner && (
               <>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,16 +20,23 @@ const Home = ({userObj}) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
-    const response = await attachmentRef.putString(attachment, "data_url");
-    const attachmentUrl = await response.ref.getDownloadURL();
-    console.log(response);
-    // dbService.collection("nweets").add({
-    //   text:nweet,
-    //   createdAt: Date.now(),
-    //   creatorId: userObj.uid
-    // });
-    // setNweet("")
+    let attachmentUrl = "";
+
+    if(attachment !="") {
+      const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+  
+    const nweetObj = {
+      text:nweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl
+    }
+    dbService.collection("nweets").add(nweetObj);
+    setNweet("")
+    setAttachment("");
   };
   const onChange = (event) => {
     const { target : {value}} = event;
